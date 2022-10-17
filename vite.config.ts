8de80b89f0205a899871feae4bd231b4bcdcb865
すえ <souhait.0614@gmail.com>
2022-10-18 08:00:27 +0900

    開発準備2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   build: {
+    target: "es2020",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "SCCoreV2",
