c70273e5020ff96a9674c5dd341e3d0762a7962f
すえ <souhait.0614@gmail.com>
2023-01-14 15:23:32 +0900

    テスト追加

--- test/index.test.ts
+++ test/index.test.ts
@@ -23,4 +23,28 @@ describe("Converter", () => {
 
     expect(result.convertedText).toEqual("THIS IS A VERY COOOOOOOOOOL LIBRARY.")
   })
+  it("FallbackConvert", async () => {
+    const errorConverter = () => {
+      throw new Error("dummy error")
+    }
+    const exampleConverter1 = (source: string) => source.toUpperCase()
+    const exampleConverter2 = (source: string) => source.replaceAll("O", "OOOOO")
+
+    const source = "This is a very cool library."
+
+    const converter = new Converter({
+      pluginList: makePluginList([
+        new Plugin({ id: "example1", converter: [errorConverter, exampleConverter1] }),
+        new Plugin({ id: "example2", converter: [exampleConverter2] }),
+      ]),
+    })
+
+    const result = await converter.convert(source, [
+      { pluginId: "example1" },
+      { pluginId: "example2" },
+    ])
+
+    expect(result.convertedText).toEqual("THIS IS A VERY COOOOOOOOOOL LIBRARY.")
+    expect(result.conversionResults[0]?.conversionError).toEqual([new Error("dummy error")])
+  })
 })
